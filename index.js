a5d3f429ed5f42a5d05ca154db5362bb37c317aa
Christian Kaisermann <christian@kaisermann.me>
2019-11-05 18:05:37 -0300

    fix: 🐛 typescript preset

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 module.exports = {
-	extends: ['eslint:recommended', 'prettier', 'prettier'],
-	plugins: ['prettier', 'import'],
-	env: {
-		es6: true,
-	},
-	parserOptions: {
-		ecmaVersion: 2018,
-	},
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? ['warn', { allow: ['info', 'warn', 'error'] }] : 'off',
-		// https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules
-		'import/order': [
-			'error',
-			{
-				'newlines-between': 'always',
-				groups: ['builtin', 'external', ['internal', 'parent'], ['sibling', 'index']],
-			},
-		],
-		'import/first': 'error',
-		'import/no-duplicates': 'error',
-		'import/newline-after-import': 'error',
-		'import/no-mutable-exports': 'warn',
-		'import/export': 'warn',
-		'import/no-useless-path-segments': 'error',
-		'import/no-self-import': 'error',
-		'import/no-absolute-path': 'error',
-	},
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier', 'import'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? ['warn', { allow: ['info', 'warn', 'error'] }]
+        : 'off',
+
+    'prefer-const': ['error', { destructuring: 'all' }],
+
+    /** Enforce file extensions on 'import' statements */
+    'import/extensions': ['error', 'always', { ignorePackages: true }],
+
+    /** Allow to import peer dependencies */
+    'import/no-extraneous-dependencies': ['warn', { peerDependencies: true }],
+
+    /** No one prefers the default export... */
+    'import/prefer-default-export': 'off',
+
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          ['internal', 'parent'],
+          ['sibling', 'index'],
+        ],
+      },
+    ],
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-mutable-exports': 'warn',
+    'import/export': 'warn',
+    'import/no-useless-path-segments': 'error',
+    'import/no-self-import': 'error',
+    'import/no-absolute-path': 'error',
+  },
 };
--- typescript.js
+++ typescript.js
@@ -1,13 +1,28 @@
 module.exports = {
-	root: true,
-	extends: ['./index.js'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		ecmaVersion: 6,
-		sourceType: 'module',
-	},
-	rules: {
-		'@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
-	},
+  root: true,
+  extends: [
+    './index.js',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 };
