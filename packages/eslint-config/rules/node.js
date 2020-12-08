269d7ad9b76de16d58de3ceeabe136aa740498dc
Cezar Augusto <boss@cezaraugusto.net>
2020-12-08 17:14:14 -0300

    Do not use deprecated eslint rules

--- packages/eslint-config/rules/node.js
+++ packages/eslint-config/rules/node.js
@@ -2,26 +2,28 @@ module.exports = {
   env: {
     node: true,
   },
+  extends: ['plugin:node/recommended'],
+  plugins: ['node'],
   rules: {
     // Disallow use of process.env
-    // https://eslint.org/docs/rules/no-process-env
-    'no-process-env': 'off',
+    // https://github.com/mysticatea/eslint-plugin-node/blob/v11.1.0/docs/rules/no-process-env.md
+    'node/no-process-env': 'error',
 
     // Enforce a callback to return
-    // https://eslint.org/docs/rules/callback-return
+    // https://github.com/mysticatea/eslint-plugin-node/blob/v11.1.0/docs/rules/callback-return.md
     //! maybe too annoying
-    'callback-return': 'off',
+    'node/callback-return': 'error',
 
     // Require all requires be top-level
-    // https://eslint.org/docs/rules/global-require
-    'global-require': 'error',
+    // https://github.com/mysticatea/eslint-plugin-node/blob/v11.1.0/docs/rules/global-require.md
+    'node/global-require': 'error',
 
     // Disallow use of new operator with the require function
-    // https://eslint.org/docs/rules/no-new-require
-    'no-new-require': 'error',
+    // https://github.com/mysticatea/eslint-plugin-node/blob/v11.1.0/docs/rules/no-new-require.md
+    'node/no-new-require': 'error',
 
     // Disallow string concatenation with __dirname and __filename
-    // https://eslint.org/docs/rules/no-path-concat
-    'no-path-concat': 'error',
+    // https://github.com/mysticatea/eslint-plugin-node/blob/v11.1.0/docs/rules/no-path-concat.md
+    'node/no-path-concat': 'error',
   },
 };
