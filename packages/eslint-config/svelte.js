d1a4d758e5fa198431a22bc7ac2bb4377f003dd0
Christian Kaisermann <christian@kaisermann.me>
2021-07-01 08:53:14 -0300

    chore: update svelte libs

--- packages/eslint-config/svelte.js
+++ packages/eslint-config/svelte.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: ['./index.js'],
   plugins: ['svelte3'],
+  env: {
+    es6: true,
+    browser: true,
+  },
   overrides: [
     {
-      files: ['**/*.svelte'],
+      files: ['*.svelte'],
       processor: 'svelte3/svelte3',
       globals: {
         $$props: true,
