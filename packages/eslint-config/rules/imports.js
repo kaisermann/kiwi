da05de8330197c0c2224e758e6b2a6c3d6b2b7fd
Christian Kaisermann <christian@kaisermann.me>
2023-03-22 10:41:34 +0100

    feat: prefer inline type imports by default

--- packages/eslint-config/rules/imports.js
+++ packages/eslint-config/rules/imports.js
@@ -1,18 +1,27 @@
-// https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules
+// https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules
 module.exports = {
   extends: ['plugin:import/typescript'],
   plugins: ['import'],
   rules: {
     // Disallow non-import statements appearing before import statements
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/first.md
     'import/first': 'error',
 
     // Disallow duplicate imports
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-    'import/no-duplicates': 'error',
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/no-duplicates.md
+    'import/no-duplicates': [
+      'error',
+      {
+        'prefer-inline': true,
+      },
+    ],
+
+    // Enforce a consistent type import style
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/consistent-type-specifier-style.md
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
 
     // Ensure native, external and internal imports are separated, above relative imports and that unassigned imports are ignored
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/order.md
     'import/order': [
       'error',
       {
@@ -43,27 +52,27 @@ module.exports = {
     ],
 
     // Require a newline after the last import/require in a group
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/newline-after-import.md
     'import/newline-after-import': 'error',
 
     // Forbid import of modules using absolute paths
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-absolute-path.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/no-absolute-path.md
     'import/no-absolute-path': 'error',
 
     // Forbid mutable exports
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-mutable-exports.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/no-mutable-exports.md
     'import/no-mutable-exports': 'error',
 
     // Disallow invalid exports, e.g. multiple defaults
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/export.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/export.md
     'import/export': 'error',
 
     // Ensures that there are no useless path segments
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-useless-path-segments.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/no-useless-path-segments.md
     'import/no-useless-path-segments': ['error', { commonjs: true }],
 
     // Forbid a module from importing itself
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-self-import.md
+    // https://github.com/import-js/eslint-plugin-import/tree/main/docs/rules/no-self-import.md
     'import/no-self-import': 'error',
   },
 };
