122f288bc51bb1ed68ce70aa36f8955b9e99dfc2
Christian Kaisermann <christian@kaisermann.me>
2025-05-19 10:26:42 +0200

    Use eslint-plugin-n instead of eslint-plugin-node

--- packages/eslint-config/rules/node.js
+++ packages/eslint-config/rules/node.js
@@ -1,53 +1,53 @@
-const { CONFIG_FILES_GlOBS } = require('../lib/consts.js');
+const { CONFIG_FILES_GlOBS } = require("../lib/consts.js");
 
 module.exports = {
-  plugins: ['node'],
+  plugins: ["n"],
   env: {
     node: true,
   },
   rules: {
     // Disallow use of process.env
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-process-env.md
-    'node/no-process-env': 'off',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-process-env.md
+    "n/no-process-env": "off",
 
     // Enforce a callback to return
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/callback-return.md
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/callback-return.md
     //! too annoying
-    'node/callback-return': 'off',
+    "n/callback-return": "off",
 
     // Require all requires be top-level
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/global-require.md
-    'node/global-require': 'error',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/global-require.md
+    "n/global-require": "error",
 
     // Disallow use of new operator with the require function
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-new-require.md
-    'node/no-new-require': 'error',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-new-require.md
+    "n/no-new-require": "error",
 
     // Disallow string concatenation with __dirname and __filename
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-path-concat.md
-    'node/no-path-concat': 'error',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-path-concat.md
+    "n/no-path-concat": "error",
 
     // Make process.exit() expressions the same code path as throw
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/process-exit-as-throw.md
-    'node/process-exit-as-throw': 'error',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/process-exit-as-throw.md
+    "n/process-exit-as-throw": "error",
 
     // Disallow deprecated APIs
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-deprecated-api.md
-    'node/no-deprecated-api': 'error',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/no-deprecated-api.md
+    "n/no-deprecated-api": "error",
 
     // Encourages use of promise APIs instead o callback APIs
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/prefer-promises/fs.md
-    // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/prefer-promises/dns.md
-    'node/prefer-promises/fs': 'warn',
-    'node/prefer-promises/dns': 'warn',
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/prefer-promises/fs.md
+    // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/prefer-promises/dns.md
+    "n/prefer-promises/fs": "warn",
+    "n/prefer-promises/dns": "warn",
   },
   overrides: [
     {
       files: CONFIG_FILES_GlOBS,
       rules: {
         // Require all requires be top-level
-        // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/global-require.md
-        'node/global-require': 'off',
+        // https://github.com/eslint-community/eslint-plugin-n/blob/master/docs/rules/global-require.md
+        "n/global-require": "off",
       },
     },
   ],
